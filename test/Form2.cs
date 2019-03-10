ac611fc1cfb8fae896fd6f9a15956c2c6cb0b7be
wearefriend888 <cc@163.com>
2019-03-10 15:10:57 +0800

    second commit

--- test/Form2.cs
+++ test/Form2.cs
@@ -9,17 +9,11 @@ using System.Windows.Forms;
 
 namespace test
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Form2 frm = new Form2();
-            frm.ShowDialog();
         }
     }
 }
