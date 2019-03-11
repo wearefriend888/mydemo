459758dc4cc17bab05c038f4b2d66ea5ba4962cb
wearefriend888 <friend2450@126.com>
2019-03-11 09:04:12 +0800

    第三次提交

--- test/Form2.cs
+++ test/Form2.cs
@@ -15,5 +15,10 @@ namespace test
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("this is test!");
+        }
     }
 }
